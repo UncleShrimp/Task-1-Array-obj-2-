fea186159c6ef5b58f9c4cc1c42d6aaa33227e2c
danaa <ivanuick@gmail.com>
2024-06-19 13:10:02 +0300

    second save

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -2,6 +2,26 @@
 
 using namespace std;
 
+bool parity(int in) {
+    if (in%2==0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 int main() {
-    
+    int array2D[20][20];
+
+    for (int y=0; y<20; y++) {
+        for (int x=0; x<20; x++) {
+            array2D[y][x] = 1+rand()%100;
+            if (parity(array2D[y][x])) {
+                array2D[y][x]*=2;
+            }
+            cout<<array2D[y][x];
+        }
+        cout<<""<<endl;
+    }
 }
