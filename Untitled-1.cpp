105def34e0cb0b6ce91e542f56ee0a9d2dbe3970
danaa <ivanuick@gmail.com>
2024-06-19 14:00:37 +0300

    fifth save

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -11,12 +11,16 @@ bool parity(int in) {
     }
 }
 
+int randomNum(int a, int b) {
+    return a+rand()%b;
+}
+
 int main() {
     int array2D[20][20];
 
     for (int y=0; y<20; y++) {
         for (int x=0; x<20; x++) {
-            array2D[y][x] = 1+rand()%100;
+            array2D[y][x] = randomNum(1, 100);
             if (parity(array2D[y][x])) {
                 //cout<<"True";
                 array2D[y][x]*=2;
